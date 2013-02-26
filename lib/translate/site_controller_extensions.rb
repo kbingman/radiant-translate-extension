d78cad53ceab4943d9b99ec517f395181dd516e0
Keith Bingman <keith@keithbingman.com>
2013-02-26 13:38:29 +0100

    revert changes

--- lib/translate/site_controller_extensions.rb
+++ lib/translate/site_controller_extensions.rb
@@ -3,49 +3,52 @@ module Translate::SiteControllerExtensions
     base.class_eval do
       before_filter :set_language
       
-      def show_page
-        url = params[:url]
-        locale = current_locale(params[:locale])
-        
-        if Array === url
-          url = url.join('/')
-        else
-          url = url.to_s
-        end
-        if @page = find_page(url) && params[:locale]
-          batch_page_status_refresh if (url == "/" || url == "")
-          process_page(@page)
-          set_cache_control
-          @performed_render ||= true
-        elsif @page = find_page(url)
-          redirect_to :locale => locale, :url => @page.path, :status => 301   
-        else
-          render :template => 'site/not_found', :status => 404
-        end
-      rescue Page::MissingRootPageError
-        redirect_to welcome_url
-      end
-      
       private
 
       def set_language 
-        I18n.locale = current_locale(params[:locale]).to_sym   
-      end   
-      
-      def current_locale locale
-        if locale_included?(locale)
-          locale
-        else
-          Radiant::Config['translate.default_site_language']
+        if params[:locale]
+          I18n.locale = params[:locale].to_sym   
+        else  
+          # I18n.locale = Radiant::Config['translate.default_site_language']      
+          # locale = location_map.include?(languages.first) ? languages.first : Radiant::Config['translate.default_site_language']
+          locale = Radiant::Config['translate.default_site_language']
+          # path = params[:url] unless params[:url] == '/'
+          # redirect_to :locale => locale, :url => path, :status => 301   
+          # languages.first
         end
-      end
-      
-      def locale_included? locale
-        if Radiant::Config['translate.site_languages'].split(',').include?(locale)
-      end
+      end   
       
     end  
     
+    protected
+
+      def languages
+        langs = (request.env["HTTP_ACCEPT_LANGUAGE"] || "").split(/[,\s]+/)
+        langs_with_weights = langs.map do |ele|
+          both = ele.split(/;q=/)
+          lang = both[0].split('-').first
+          weight = both[1] ? Float(both[1]) : 1
+          [-weight, lang]
+        end.sort_by(&:first).map(&:last) 
+        langs_with_weights
+      end
+
+      def location
+        path = location_map.include?(languages.first) ? languages.first : nil
+        path ||= "/#{Radiant::Config['translate.default_site_language']}/"
+        path += request.request_uri
+        path.gsub!(%r{([^:])//}, '\1/')
+        if path =~ %r{[:][/][/]}
+          path
+        else
+          path.sub!(%r{^([^/])}, '/\1')
+          # @request.protocol + @request.host_with_port + path
+        end
+      end
+
+      def location_map
+        @location_map ||= Radiant::Config['translate.site_languages'].split(',').collect{ |l| l.strip }
+      end
   end
   
 end
